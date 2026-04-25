0b8371e76ef9ef4ed9490bc93e20ee63b0c9bdd7
karlmal20 <karleymalveo@yahoo.com>
2026-04-25 00:26:11 -0500

    Add function to generate time travel message

---
+++ custom_module.py
@@ -0,0 +1,4 @@
+# Add your Code below
+def generate_time_travel_message(year, destination, cost):
+  message = 'You are going to ' + destination + ' in ' + str(year) + '. It will cost $' + str(cost)
+  return message
